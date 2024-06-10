7c71554e4d4f99bc090c24b3e31f61195c8b5306
wahyu <wahyumuhari178@gmail.com>
2024-06-10 22:12:33 +0700

    Menambahkan file index2.php

---
+++ index2.php
@@ -0,0 +1,76 @@
+<html>
+<title>Index 2</title>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+
+    <div class="container d-flex justify-content-center align-items-center">
+
+        <div class="card">
+
+            <div class="upper">
+
+                <img src="https://i.imgur.com/Qtrsrk5.jpg" class="img-fluid">
+
+            </div>
+
+            <div class="user text-center">
+
+                <div class="profile">
+
+                    <img src="gambar.png" class="rounded-circle" width="100px" height="100px">
+
+                </div>
+
+            </div>
+
+
+            <div class="mt-5 text-center">
+
+                <h4 class="mb-0">Benjamin Tims</h4>
+                <span class="text-muted d-block mb-2">Los Angles</span>
+
+                <button class="btn btn-primary btn-sm follow">Follow</button>
+
+
+                <div class="d-flex justify-content-between align-items-center mt-4 px-4">
+
+                    <div class="stats">
+                        <h6 class="mb-0">Followers</h6>
+                        <span>8,797</span>
+
+                    </div>
+
+
+                    <div class="stats">
+                        <h6 class="mb-0">Projects</h6>
+                        <span>142</span>
+
+                    </div>
+
+
+                    <div class="stats">
+                        <h6 class="mb-0">Ranks</h6>
+                        <span>129</span>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
